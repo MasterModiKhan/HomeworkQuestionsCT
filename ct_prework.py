adb94d02f6eb43f715e92174f90607319cd6f0bb
MasterModiKhan <135379231+MasterModiKhan@users.noreply.github.com>
2023-06-11 16:25:24 -0400

    Add files via upload

---
+++ ct_prework.py
@@ -0,0 +1,54 @@
+#question 1 #notes: i am achieving desired output but also receiving None on questions 1 and 2
+#def hello_USERNAME(username):
+#    username = input("Enter Name: ")
+#    print("Username is: " + username)
+#print(hello_USERNAME(username=" "))
+
+
+
+# question 2
+#def first_odds():
+#    for i in range (1,100):
+#        print(i)
+#print(first_odds())
+
+
+# question 3 
+#def max_num_in_list(a_list):
+#    max = a_list [1]
+#    for a in a_list:
+#        if a > max:
+#            max = a
+#            return max
+#a_list = list(input("Enter list items: ").split())
+#
+#print(max_num_in_list(a_list))
+
+
+
+# question 4 
+#def is_leap_year(year):
+#    year = int(input("Enter a year: "))
+#    if((year % 400) or
+#        (year % 100 != 0) and
+#        (year % 4 == 0)):
+#        return True
+#    else:
+#        return False
+#print(is_leap_year(year=' '))
+    
+
+       
+
+
+
+# question 5 
+#def is_consecutive(a_list):
+#        x = a_list[:]
+#        x.sort()
+#        if a_list == x:
+#            return True
+#        else: 
+#            return False
+#a_list=list(input("Enter list items : ").split())
+#print(is_consecutive(a_list))
